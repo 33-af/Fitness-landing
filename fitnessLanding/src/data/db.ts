9ee7cc8e47a2a83de5d8a3ce76eb40074178a54f
Mykhail <gorgefraggggg@gmail.com>
2025-04-03 11:43:20 +0300

    final updated all  initial code

---
+++ fitnessLanding/src/data/db.ts
@@ -0,0 +1,24 @@
+const data = [
+    {
+     question: 'What is FitMaker and how can it help me reach my fitness goals?',
+     answer: 'FitMaker Is An Online Fitness Platform That Offers Personalized Workout Plans, Expert Coaching, And Comprehensive Nutritional Guidance. Whether YouRe Looking To Lose Weight, Build Muscle, Or Simply Stay Fit, Our Tailored Programs And Community Support Will Help You Achieve Your Fitness Goals.' ,
+    },
+    {
+     question: 'How do I get started with a workout plan on FitMaker?',
+     answer: 'FitMaker Is An Online Fitness Platform That Offers Personalized Workout Plans, Expert Coaching, And Comprehensive Nutritional Guidance. Whether YouRe Looking To Lose Weight, Build Muscle, Or Simply Stay Fit, Our Tailored Programs And Community Support Will Help You Achieve Your Fitness Goals.' ,
+    },
+    {
+     question: 'What is included in the Custom Plan?',
+     answer: 'FitMaker Is An Online Fitness Platform That Offers Personalized Workout Plans, Expert Coaching, And Comprehensive Nutritional Guidance. Whether YouRe Looking To Lose Weight, Build Muscle, Or Simply Stay Fit, Our Tailored Programs And Community Support Will Help You Achieve Your Fitness Goals.' ,
+    },
+    {
+     question: 'Can I change my plan after signing up?',
+     answer: 'FitMaker Is An Online Fitness Platform That Offers Personalized Workout Plans, Expert Coaching, And Comprehensive Nutritional Guidance. Whether YouRe Looking To Lose Weight, Build Muscle, Or Simply Stay Fit, Our Tailored Programs And Community Support Will Help You Achieve Your Fitness Goals.' ,
+    },
+    {
+        question: 'What kind of support can I expect from my trainer?',
+        answer: 'FitMaker Is An Online Fitness Platform That Offers Personalized Workout Plans, Expert Coaching, And Comprehensive Nutritional Guidance. Whether YouRe Looking To Lose Weight, Build Muscle, Or Simply Stay Fit, Our Tailored Programs And Community Support Will Help You Achieve Your Fitness Goals.' ,
+    }
+   ];
+  
+   export default data;
